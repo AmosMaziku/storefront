3695249e8b2af51e28eb60741864bd7bbe404a8f
Maziku <salomejuma123@gmail.com>
2023-01-10 07:33:32 +0300

    Fixing errors and adding New model

--- tags/models.py
+++ tags/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from django.contrib.contenttypes.models import ContentTypes 
+from django.contrib.contenttypes.models import ContentType 
 from django.contrib.contenttypes.fields import GenericForeignKey
 
 class Tag(models.Model):
-    title = models.CharField(max_legnth=255)
+    title = models.CharField(max_length=255)
 
 
 class TggedItem(models.Model):
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    content_type = models.ForeignKey(ContentTypes, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
